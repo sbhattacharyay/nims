98ab4c0783ded3d1bba45a46f978a3b010d2a361
shubhayubh <60406729+sbhattacharyay@users.noreply.github.com>
2020-03-05 18:44:07 -0500

    Organize scripts

--- functions/setFigureFormat.m
+++
@@ -1,7 +0,0 @@
-function setFigureFormat(inputArg1,inputArg2)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
