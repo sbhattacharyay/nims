98748f2b38be817afa38eb73e0ef4d510b279dcb
shubhayubh <60406729+sbhattacharyay@users.noreply.github.com>
2020-03-05 22:00:53 -0500

    Add scripts

---
+++ scripts/noMotion_th.m
@@ -0,0 +1,15 @@
+function nm_A = noMotion_th(A,th)
+%   Finds percentage of values in each row that are below threshold
+dimA = size(A);
+nm_A = NaN(dimA(1),1);
+for i = 1:dimA(1)
+    currRow = A(i,:);
+    if sum(~isnan(currRow)) == 0
+        nm_A(i)=NaN;
+        continue
+    else
+        row_pi=(sum(currRow < th))/(sum(~isnan(currRow)));
+        nm_A(i)=row_pi;
+    end
+end
+end
