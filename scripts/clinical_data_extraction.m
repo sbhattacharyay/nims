4c168de9d6c227dc7b962e94af93e6b4582de344
shubhayubh <60406729+sbhattacharyay@users.noreply.github.com>
2020-03-13 00:02:08 -0400

    Update Missing Imputation Script

--- scripts/clinical_data_extraction.m
+++ scripts/clinical_data_extraction.m
@@ -33,7 +33,6 @@ studyPatients = [2	3	4	5	6	7	8	9	10	11	12	13	14	15 ...
     16	17	18	19	20	21	22	23	24	26	27	28	29	30	31	32	33 ....
     34	35	36	37	38	39	40	41	42	43	44	45	46	47	48	49	50 .....
     51	52	53	54	55	58	59	60	61	62	63	64	66	67];
-
 %% Extract Outcome Information from Patient Dataset
 
 fav_threshold = 4; % GOSE >= fav_threshold is favorable
@@ -136,6 +135,9 @@ clearvars -except dc_dataset yr_dataset dc_dataset_labels ...
     imputeDataset studyPatients
 
 save('clinical_extraction_output.mat')
+
+cd ..
+cd scripts
 %% Visualize distribution of transformed (Box-Cox) clinical variables
 figure
 for i =1:5
