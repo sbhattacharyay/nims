c0fd5ad62a30ba54e1ae5034cf8a52a787e6d3a8
shubhayubh <60406729+sbhattacharyay@users.noreply.github.com>
2020-03-26 21:14:58 -0700

    updates to imputation regression model

---
+++ scripts/functions/wrist_elbow_Regression.m
@@ -0,0 +1,59 @@
+function patient_table_output = wrist_elbow_Regression(patient_table,...
+    wrist_Idx,elbow_Idx,totallyMissingIdxs)
+
+patient_table_output=patient_table;
+
+% Totally missing indices with a missing arm (elbow or wrist) parameter
+arm_TMI = totallyMissingIdxs(:,ismember(totallyMissingIdxs(1,:),[wrist_Idx,elbow_Idx]));
+temp_dim = size(arm_TMI);
+
+for i=1:temp_dim(2)
+    curr_sensorIdx = arm_TMI(1,i);
+    curr_featureIdx = arm_TMI(2,i);
+    if ismember(curr_sensorIdx,wrist_Idx)
+        
+        responseVar1 = ['pi_' char(feature_names(curr_featureIdx)) '_' ...
+            num2str(curr_sensorIdx)];
+        responseVar2 = ['lambda_' char(feature_names(curr_featureIdx)) '_' ...
+            num2str(curr_sensorIdx)];
+        predictorVar1 = ['pi_' char(feature_names(curr_featureIdx)) '_' ...
+            num2str(curr_sensorIdx-1)];
+        predictorVar2 = ['lambda_' char(feature_names(curr_featureIdx)) '_' ...
+            num2str(curr_sensorIdx-1)];
+        mdl1=fitglm(patient_table,'Distribution','binomial','ResponseVar',...
+            responseVar1,'PredictorVars',predictorVar1);
+        mdl2=fitglm(patient_table,'Distribution','poisson','ResponseVar',...
+            responseVar2,'PredictorVars',predictorVar2);
+        curr_miss_Idx1 = isnan(patient_table.(responseVar1));
+        curr_miss_Idx2 = isnan(patient_table.(responseVar2));
+        
+        patient_table_output{curr_miss_Idx1,responseVar1}=predict(mdl1,...
+            patient_table(curr_miss_Idx1,{predictorVar1}));
+        patient_table_output{curr_miss_Idx2,responseVar2}=predict(mdl2,...
+            patient_table(curr_miss_Idx2,{predictorVar2}));        
+        
+    elseif ismember(curr_sensorIdx,elbow_Idx)
+        responseVar1 = ['pi_' char(feature_names(curr_featureIdx)) '_' ...
+            num2str(curr_sensorIdx)];
+        responseVar2 = ['lambda_' char(feature_names(curr_featureIdx)) '_' ...
+            num2str(curr_sensorIdx)];
+        predictorVar1 = ['pi_' char(feature_names(curr_featureIdx)) '_' ...
+            num2str(curr_sensorIdx+1)];
+        predictorVar2 = ['lambda_' char(feature_names(curr_featureIdx)) '_' ...
+            num2str(curr_sensorIdx+1)];
+        mdl1=fitglm(patient_table,'Distribution','binomial','ResponseVar',...
+            responseVar1,'PredictorVars',predictorVar1);
+        mdl2=fitglm(patient_table,'Distribution','poisson','ResponseVar',...
+            responseVar2,'PredictorVars',predictorVar2);
+        curr_miss_Idx1 = isnan(patient_table.(responseVar1));
+        curr_miss_Idx2 = isnan(patient_table.(responseVar2));
+        
+        patient_table_output{curr_miss_Idx1,responseVar1}=predict(mdl1,...
+            patient_table(curr_miss_Idx1,{predictorVar1}));
+        patient_table_output{curr_miss_Idx2,responseVar2}=predict(mdl2,...
+            patient_table(curr_miss_Idx2,{predictorVar2}));   
+        
+    end
+end
+end
+
--- scripts/missing_data_imputation.m
+++ scripts/missing_data_imputation.m
@@ -99,51 +99,8 @@ wrist_Idx = [4,7];
 % elbow indices (defult 3 and 6):
 elbow_Idx = [3,6];
 
-% Totally missing indices with a missing arm (elbow or wrist) parameter
-arm_TMI = totallyMissingIdxs(:,ismember(totallyMissingIdxs(1,:),[wrist_Idx,elbow_Idx]));
-temp_dim = size(arm_TMI);
-
-for i=1:temp_dim(2)
-    curr_sensorIdx = arm_TMI(1,i);
-    curr_featureIdx = arm_TMI(2,i);
-    if ismember(curr_sensorIdx,wrist_Idx)
-        
-        responseVar1 = ['pi_' char(feature_names(curr_featureIdx)) '_' ...
-            num2str(curr_sensorIdx)];
-        responseVar2 = ['lambda_' char(feature_names(curr_featureIdx)) '_' ...
-            num2str(curr_sensorIdx)];
-        predictorVar1 = ['pi_' char(feature_names(curr_featureIdx)) '_' ...
-            num2str(curr_sensorIdx-1)];
-        predictorVar2 = ['lambda_' char(feature_names(curr_featureIdx)) '_' ...
-            num2str(curr_sensorIdx-1)];
-        mdl1=fitglm(patient_table,'Distribution','binomial','ResponseVar',...
-            responseVar1,'PredictorVars',predictorVar1);
-        mdl2=fitglm(patient_table,'Distribution','binomial','ResponseVar',...
-            responseVar2,'PredictorVars',predictorVar2);
-        curr_miss_Idx1 = isnan(patient_table.(responseVar1));
-        curr_miss_Idx2 = isnan(patient_table.(responseVar2));
-        
-        patient_table{curr_miss_Idx1,responseVar1}=predict(mdl1,...
-            patient_table(curr_miss_Idx1,{predictorVar1}));
-        patient_table{curr_miss_Idx2,responseVar2}=predict(mdl2,...
-            patient_table(curr_miss_Idx2,{predictorVar2}));        
-        
-    elseif ismember(curr_sensorIdx,elbow_Idx)
-        responseVar1 = ['pi_' char(feature_names(curr_featureIdx)) '_' ...
-            num2str(curr_sensorIdx)];
-        responseVar2 = ['lambda_' char(feature_names(curr_featureIdx)) '_' ...
-            num2str(curr_sensorIdx)];
-        predictorVar1 = ['pi_' char(feature_names(curr_featureIdx)) '_' ...
-            num2str(curr_sensorIdx+1)];
-        predictorVar2 = ['lambda_' char(feature_names(curr_featureIdx)) '_' ...
-            num2str(curr_sensorIdx+1)];
-        mdl1=fitglm(patient_table,'Distribution','binomial','ResponseVar',...
-            responseVar1,'PredictorVars',predictorVar1);
-        mdl2=fitglm(patient_table,'Distribution','binomial','ResponseVar',...
-            responseVar2,'PredictorVars',predictorVar2);
-        
-    end
-end
+patient_table=wrist_elbow_Regression(patient_table,wrist_Idx,elbow_Idx,...
+    totallyMissingIdxs);
 
 % - for leg and bed, i will need to try something else
 
