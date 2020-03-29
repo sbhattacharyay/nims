7394c2b6f2961780301fa083471d022f562c69dc
shubhayubh <60406729+sbhattacharyay@users.noreply.github.com>
2020-03-29 01:00:56 -0700

    add remaining missing data imputation functionality

---
+++ scripts/functions/characterize_missing_data.m
@@ -0,0 +1,42 @@
+function [missing_percentages,missing_time_series,missingIdxs] = ...
+    characterize_missing_data(sensors,t,studyPatientsPY,display_plots)
+
+dim_of_sensors=size(sensors);
+
+missing_percentages=cellfun(@(x) sum(isnan(x),2)/length(t),sensors,...
+    'UniformOutput',false);
+
+stacked_matrix=[];
+sensors_missing=zeros(length(studyPatientsPY),length(t));
+
+for i = 1:(dim_of_sensors(1))
+    curr_matrix = sensors{i,6};
+    stacked_matrix = [stacked_matrix; curr_matrix];
+    sensors_missing = sensors_missing+isnan(curr_matrix);
+end
+
+missingID=[0 NaN Inf];
+
+missing_time_series=sum(isnan(stacked_matrix))/(length(studyPatientsPY)*...
+    (dim_of_sensors(1)-1));
+missingIdxs=cellfun(@(x) ismissing(x,missingID),sensors,'UniformOutput',false);
+
+if display_plots == true
+    figure
+    x = [t(1) t(end)];
+    y = [1 length(studyPatientsPY)];
+    colormap(flipud(pink))
+    
+    image(x,y,sensors_missing,'CDataMapping','scaled')
+    ax = gca;
+    ax.FontSize = 14;
+    datetick('x','HH PM');
+    set(gca, 'FontName', 'Myriad Pro');
+    h=colorbar;
+    ylabel(h, 'No. of Sensors Missing','FontSize',14)
+    set(gcf, 'Units', 'Inches', 'Position', [0, 0, 10, 10], 'PaperUnits', 'Inches', 'PaperSize', [10, 10])
+    xlabel('Time of Day','FontWeight','bold','FontSize',20);
+    ylabel('Patient No.','FontWeight','bold','FontSize',20);
+end
+end
+
