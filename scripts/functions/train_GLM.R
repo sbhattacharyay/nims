84e831588059d970234b34d7da89f6d9fbd7d23e
shubhayubh <60406729+sbhattacharyay@users.noreply.github.com>
2020-04-25 17:11:29 -0700

    update glm classification

--- scripts/functions/train_GLM.R
+++ scripts/functions/train_GLM.R
@@ -1,9 +1,13 @@
-train_GLM<-function(train_CV,Y,gose_logical,currTrainIdx){
-  
-  if (gose_logical == TRUE){
-  
-    
-  } else {
+train_GLM<-function(train_CV,Y,currTrainIdx,a,k){
 
-  }
+    bp_glm<-cv.glmnet(train_CV$band_power_train_cv,Y[currTrainIdx],alpha = a,family = "binomial",type.measure = "auc",nfolds=k)
+    fe_glm<-cv.glmnet(train_CV$band_power_train_cv,Y[currTrainIdx],alpha = a,family = "binomial",type.measure = "auc",nfolds=k)
+    fp_glm<-cv.glmnet(train_CV$band_power_train_cv,Y[currTrainIdx],alpha = a,family = "binomial",type.measure = "auc",nfolds=k)
+    mf_glm<-cv.glmnet(train_CV$band_power_train_cv,Y[currTrainIdx],alpha = a,family = "binomial",type.measure = "auc",nfolds=k)
+    sm_glm<-cv.glmnet(train_CV$band_power_train_cv,Y[currTrainIdx],alpha = a,family = "binomial",type.measure = "auc",nfolds=k)
+    wv_glm<-cv.glmnet(train_CV$band_power_train_cv,Y[currTrainIdx],alpha = a,family = "binomial",type.measure = "auc",nfolds=k)
+    
+    tempList<-list(bp_glm,fe_glm,fp_glm,mf_glm,sm_glm,wv_glm)
+    names(tempList)<-c("bp_glm","fe_glm","fp_glm","mf_glm","sm_glm","wv_glm")
+    return(tempList)
 } 
--- scripts/master9_glm_classification.R
+++ scripts/master9_glm_classification.R
@@ -9,7 +9,10 @@
 # email address: shubhayu@jhu.edu
 
 library(devtools)
-if (!require(lolR)) install_github('neurodata/lol', build_vignettes=TRUE, force=TRUE)
+if (!require(lolR))
+  install_github('neurodata/lol',
+                 build_vignettes = TRUE,
+                 force = TRUE)
 library(lolR)
 library(R.matlab)
 library(tidyverse)
@@ -17,6 +20,7 @@ library(ggplot2)
 library(plotly)
 library(naniar)
 library(MASS)
+library(glmnet)
 
 source('./functions/load_patient_clinical_data.R')
 source('./functions/update_clinicalVariableList.R')
@@ -29,26 +33,30 @@ source("./functions/load_tf_patient_covariates.R")
 source('./functions/cv_lol_project_motion_features.R')
 source('./functions/prepare_training_covariates.R')
 source('./functions/prepare_testing_covariates.R')
+source('./functions/train_GLM.R')
+source('./functions/predict_GLM.R')
 
 # Load patient clinical data (sorts by PY numbering and corrects variable types)
-patient_clinical_data<-load_patient_clinical_data()
+patient_clinical_data <- load_patient_clinical_data()
 
 # Load and update clinical variable list
-clinicalVariableList<-update_clinicalVariableList()
+clinicalVariableList <- update_clinicalVariableList()
 
 # Load Motion Features Organized by Time of Day (TOD)
-if(!exists("tod_sensors")) {
-  tod_sensors<-readMat('../motion_feature_data/bed_corrected_imputed_complete_sensor_data.mat')$bed.corrected.sensors
+if (!exists("tod_sensors")) {
+  tod_sensors <-
+    readMat('../motion_feature_data/bed_corrected_imputed_complete_sensor_data.mat')$bed.corrected.sensors
 }
 
 # Load Motion Features Organized by Time from Recording (TFR)
-if(!exists("tfr_sensors")) {
-  tfr_sensors<-readMat('../tfr_motion_feature_data/bed_corrected_imputed_complete_sensor_data.mat')$bed.corrected.sensors
+if (!exists("tfr_sensors")) {
+  tfr_sensors <-
+    readMat('../tfr_motion_feature_data/bed_corrected_imputed_complete_sensor_data.mat')$bed.corrected.sensors
 }
 
 # Organizing Features for both motion feature categorizations
-tod_temp_var<-do.call(rbind,tod_sensors)
-tfr_temp_var<-do.call(rbind,tfr_sensors)
+tod_temp_var <- do.call(rbind, tod_sensors)
+tfr_temp_var <- do.call(rbind, tfr_sensors)
 # ranked_temp_var<-lapply(temp_var, function(x) t(apply(x,1,sort,decreasing = TRUE)))
 # fft_temp_var<-lapply(temp_var, function(x) abs(t(mvfft(t(x)))))
 
@@ -56,90 +64,204 @@ tod_motion_features <- get_motion_features(tod_temp_var)
 tfr_motion_features <- get_motion_features(tfr_temp_var)
 
 # Load transformed covariates for both TOD and TFR
-tod_tf_covariates<-load_tf_patient_covariates('../motion_feature_data/tf_patient_covariates.csv')
-tfr_tf_covariates<-load_tf_patient_covariates('../tfr_motion_feature_data/tf_patient_covariates.csv')
+tod_tf_covariates <-
+  load_tf_patient_covariates('../motion_feature_data/tf_patient_covariates.csv')
+tfr_tf_covariates <-
+  load_tf_patient_covariates('../tfr_motion_feature_data/tf_patient_covariates.csv')
 
 # Split for k-fold cross validation for discharge predictions:
-k<-5
-cvIdx<-cross_val_splits(patient_clinical_data,k)
+k <- 5
+cvIdx <- cross_val_splits(patient_clinical_data, k)
 
-for (i in 1:length(cvIdx)){
-  currTestIdx<-cvIdx[[as.character(i)]]
-  currTrainIdx<-seq(nrow(patient_clinical_data))[-currTestIdx]
+self_val_predictionsDis <- vector(mode = "list", length = k)
+test_val_predictionsDis <- vector(mode = "list", length = k)
+
+for (i in 1:length(cvIdx)) {
+  currTestIdx <- cvIdx[[as.character(i)]]
+  currTrainIdx <- seq(nrow(patient_clinical_data))[-currTestIdx]
   
   # Convert numeric to logical indexing
-  logicalTotal <- rep(FALSE,nrow(patient_clinical_data))
+  logicalTotal <- rep(FALSE, nrow(patient_clinical_data))
   logicalTest <- logicalTotal
   logicalTest[currTestIdx] <- TRUE
   logicalTrain <- !logicalTest
-
-  # Extract training and testing covariates based on current split
-  train_tfr_tf_covariates<-tfr_tf_covariates[currTrainIdx,]
-  test_tfr_tf_covariates<-tfr_tf_covariates[currTestIdx,]
   
-  train_tod_tf_covariates<-tod_tf_covariates[currTrainIdx,]
-  test_tod_tf_covariates<-tod_tf_covariates[currTestIdx,]
+  # Extract training and testing covariates based on current split
+  train_tfr_tf_covariates <- tfr_tf_covariates[currTrainIdx, ]
+  test_tfr_tf_covariates <- tfr_tf_covariates[currTestIdx, ]
+  
+  train_tod_tf_covariates <- tod_tf_covariates[currTrainIdx, ]
+  test_tod_tf_covariates <- tod_tf_covariates[currTestIdx, ]
   
   # Perform LOL on training data
-  r<-3
-  Y<-as.factor(patient_clinical_data$gose)
-  tod_GOSE_LOL<-cv_lol_project_motion_features(tod_motion_features,Y,r,logicalTrain)
-  tfr_GOSE_LOL<-cv_lol_project_motion_features(tfr_motion_features,Y,r,logicalTrain)
-
-  Y<-as.factor(patient_clinical_data$favorable)
-  tod_fav_LOL<-cv_lol_project_motion_features(tod_motion_features,Y,r,logicalTrain)
-  tfr_fav_LOL<-cv_lol_project_motion_features(tfr_motion_features,Y,r,logicalTrain)
+  r <- 3
+  Y <- as.factor(patient_clinical_data$gose)
+  tod_GOSE_LOL <-
+    cv_lol_project_motion_features(tod_motion_features, Y, r, logicalTrain)
+  tfr_GOSE_LOL <-
+    cv_lol_project_motion_features(tfr_motion_features, Y, r, logicalTrain)
   
-  Y<-as.factor(patient_clinical_data$death)
-  tod_death_LOL<-cv_lol_project_motion_features(tod_motion_features,Y,r,logicalTrain)
-  tfr_death_LOL<-cv_lol_project_motion_features(tfr_motion_features,Y,r,logicalTrain)
+  Y <- as.factor(patient_clinical_data$favorable)
+  tod_fav_LOL <-
+    cv_lol_project_motion_features(tod_motion_features, Y, r, logicalTrain)
+  tfr_fav_LOL <-
+    cv_lol_project_motion_features(tfr_motion_features, Y, r, logicalTrain)
+  
+  Y <- as.factor(patient_clinical_data$death)
+  tod_death_LOL <-
+    cv_lol_project_motion_features(tod_motion_features, Y, r, logicalTrain)
+  tfr_death_LOL <-
+    cv_lol_project_motion_features(tfr_motion_features, Y, r, logicalTrain)
   
   # Prepare training covariates
-  train_tod_GOSE_CV<- prepare_training_covariates(tod_GOSE_LOL,train_tod_tf_covariates,TRUE)
-  train_tod_fav_CV<-  prepare_training_covariates(tod_fav_LOL,train_tod_tf_covariates,TRUE)
-  train_tod_death_CV<-prepare_training_covariates(tod_death_LOL,train_tod_tf_covariates,FALSE)
+  train_tod_GOSE_CV <-
+    prepare_training_covariates(tod_GOSE_LOL, train_tod_tf_covariates, TRUE)
+  train_tod_fav_CV <-
+    prepare_training_covariates(tod_fav_LOL, train_tod_tf_covariates, TRUE)
+  train_tod_death_CV <-
+    prepare_training_covariates(tod_death_LOL, train_tod_tf_covariates, FALSE)
   
-  train_tfr_GOSE_CV<- prepare_training_covariates(tfr_GOSE_LOL,train_tfr_tf_covariates,TRUE)
-  train_tfr_fav_CV<-  prepare_training_covariates(tfr_fav_LOL,train_tfr_tf_covariates,TRUE)
-  train_tfr_death_CV<-prepare_training_covariates(tfr_death_LOL,train_tfr_tf_covariates,FALSE)
+  train_tfr_GOSE_CV <-
+    prepare_training_covariates(tfr_GOSE_LOL, train_tfr_tf_covariates, TRUE)
+  train_tfr_fav_CV <-
+    prepare_training_covariates(tfr_fav_LOL, train_tfr_tf_covariates, TRUE)
+  train_tfr_death_CV <-
+    prepare_training_covariates(tfr_death_LOL, train_tfr_tf_covariates, FALSE)
   
   # Prepare testing covariates
-  test_tod_GOSE_CV<- prepare_testing_covariates(tod_motion_features,tod_GOSE_LOL,test_tod_tf_covariates,currTestIdx,TRUE)
-  test_tod_fav_CV<-  prepare_testing_covariates(tod_motion_features,tod_fav_LOL,test_tod_tf_covariates,currTestIdx,TRUE)
-  test_tod_death_CV<-prepare_testing_covariates(tod_motion_features,tod_death_LOL,test_tod_tf_covariates,currTestIdx,FALSE)
+  test_tod_GOSE_CV <-
+    prepare_testing_covariates(tod_motion_features,
+                               tod_GOSE_LOL,
+                               test_tod_tf_covariates,
+                               currTestIdx,
+                               TRUE)
+  test_tod_fav_CV <-
+    prepare_testing_covariates(tod_motion_features,
+                               tod_fav_LOL,
+                               test_tod_tf_covariates,
+                               currTestIdx,
+                               TRUE)
+  test_tod_death_CV <-
+    prepare_testing_covariates(tod_motion_features,
+                               tod_death_LOL,
+                               test_tod_tf_covariates,
+                               currTestIdx,
+                               FALSE)
   
-  test_tfr_GOSE_CV<- prepare_testing_covariates(tfr_motion_features,tfr_GOSE_LOL,test_tfr_tf_covariates,currTestIdx,TRUE)
-  test_tfr_fav_CV<-  prepare_testing_covariates(tfr_motion_features,tfr_fav_LOL,test_tfr_tf_covariates,currTestIdx,TRUE)
-  test_tfr_death_CV<-prepare_testing_covariates(tfr_motion_features,tfr_death_LOL,test_tfr_tf_covariates,currTestIdx,FALSE)
+  test_tfr_GOSE_CV <-
+    prepare_testing_covariates(tfr_motion_features,
+                               tfr_GOSE_LOL,
+                               test_tfr_tf_covariates,
+                               currTestIdx,
+                               TRUE)
+  test_tfr_fav_CV <-
+    prepare_testing_covariates(tfr_motion_features,
+                               tfr_fav_LOL,
+                               test_tfr_tf_covariates,
+                               currTestIdx,
+                               TRUE)
+  test_tfr_death_CV <-
+    prepare_testing_covariates(tfr_motion_features,
+                               tfr_death_LOL,
+                               test_tfr_tf_covariates,
+                               currTestIdx,
+                               FALSE)
   
   # Train GLM on training data
-  Y<-as.factor(patient_clinical_data$favorable)
-  tod_GOSE_LOL<-cv_lol_project_motion_features(tod_motion_features,Y,r,logicalTrain)
-  tfr_GOSE_LOL<-cv_lol_project_motion_features(tfr_motion_features,Y,r,logicalTrain)
+  Y <- as.factor(patient_clinical_data$favorable)
   
-  tod_fav_LOL<-cv_lol_project_motion_features(tod_motion_features,Y,r,logicalTrain)
-  tfr_fav_LOL<-cv_lol_project_motion_features(tfr_motion_features,Y,r,logicalTrain)
+  tod_GOSE_GLM <- train_GLM(train_tod_GOSE_CV, Y, currTrainIdx, 0, 4)
+  tfr_GOSE_GLM <- train_GLM(train_tfr_GOSE_CV, Y, currTrainIdx, 0, 4)
   
-  Y<-as.factor(patient_clinical_data$death)
-  tod_death_LOL<-cv_lol_project_motion_features(tod_motion_features,Y,r,logicalTrain)
-  tfr_death_LOL<-cv_lol_project_motion_features(tfr_motion_features,Y,r,logicalTrain)
+  tod_fav_GLM <- train_GLM(train_tod_fav_CV, Y, currTrainIdx, 0, 4)
+  tfr_fav_GLM <- train_GLM(train_tfr_fav_CV, Y, currTrainIdx, 0, 4)
+  
+  Y <- as.factor(patient_clinical_data$death)
+  
+  tod_death_GLM <- train_GLM(train_tod_death_CV, Y, currTrainIdx, 0, 4)
+  tfr_death_GLM <- train_GLM(train_tfr_death_CV, Y, currTrainIdx, 0, 4)
+  
+  # Self-validate the GLM models on training data
+  
+  Y <- as.factor(patient_clinical_data$favorable)
+  
+  tod_GOSE_self_val <-
+    predict_GLM(tod_GOSE_GLM, train_tod_GOSE_CV, Y, currTrainIdx, TRUE)
+  tfr_GOSE_self_val <-
+    predict_GLM(tfr_GOSE_GLM, train_tfr_GOSE_CV, Y, currTrainIdx, TRUE)
+  
+  tod_fav_self_val <-
+    predict_GLM(tod_fav_GLM, train_tod_fav_CV, Y, currTrainIdx, TRUE)
+  tfr_fav_self_val <-
+    predict_GLM(tfr_fav_GLM, train_tfr_fav_CV, Y, currTrainIdx, TRUE)
+  
+  Y <- as.factor(patient_clinical_data$death)
+  
+  tod_death_self_val <-
+    predict_GLM(tod_death_GLM, train_tod_death_CV, Y, currTrainIdx, TRUE)
+  tfr_death_self_val <-
+    predict_GLM(tfr_death_GLM, train_tfr_death_CV, Y, currTrainIdx, TRUE)
+  
+  temp_self_val_list <-
+    list(
+      tod_GOSE_self_val,
+      tfr_GOSE_self_val,
+      tod_fav_self_val,
+      tfr_fav_self_val,
+      tod_death_self_val,
+      tfr_death_self_val
+    )
+  names(temp_self_val_list) <-
+    c(
+      "tod_GOSE_self_val",
+      "tfr_GOSE_self_val",
+      "tod_fav_self_val",
+      "tfr_fav_self_val",
+      "tod_death_self_val",
+      "tfr_death_self_val"
+    )
+  self_val_predictionsDis[[i]] <- temp_self_val_list
+  
+  # Predict outcomes for testing data
+  
+  Y <- as.factor(patient_clinical_data$favorable)
+  
+  tod_GOSE_test_val <-
+    predict_GLM(tod_GOSE_GLM, test_tod_GOSE_CV, Y, currTestIdx, FALSE)
+  tfr_GOSE_test_val <-
+    predict_GLM(tfr_GOSE_GLM, test_tfr_GOSE_CV, Y, currTestIdx, FALSE)
+  
+  tod_fav_test_val <-
+    predict_GLM(tod_fav_GLM, test_tod_fav_CV, Y, currTestIdx, FALSE)
+  tfr_fav_test_val <-
+    predict_GLM(tfr_fav_GLM, test_tfr_fav_CV, Y, currTestIdx, FALSE)
+  
+  Y <- as.factor(patient_clinical_data$death)
+  
+  tod_death_test_val <-
+    predict_GLM(tod_death_GLM, test_tod_death_CV, Y, currTestIdx, FALSE)
+  tfr_death_test_val <-
+    predict_GLM(tfr_death_GLM, test_tfr_death_CV, Y, currTestIdx, FALSE)
+  
+  temp_test_val_list <-
+    list(
+      tod_GOSE_test_val,
+      tfr_GOSE_test_val,
+      tod_fav_test_val,
+      tfr_fav_test_val,
+      tod_death_test_val,
+      tfr_death_test_val
+    )
+  names(temp_test_val_list) <-
+    c(
+      "tod_GOSE_test_val",
+      "tfr_GOSE_test_val",
+      "tod_fav_test_val",
+      "tfr_fav_test_val",
+      "tod_death_test_val",
+      "tfr_death_test_val"
+    )
+  test_val_predictionsDis[[i]] <- temp_test_val_list
 }
 
 # Split for k-fold cross validation for 12 month predictions:
-k<-5
-cvIdx<-cross_val_splits(patient_clinical_data,k)
-
-
-Y<-as.factor(patient_clinical_data$death_12mo)
-tod_death12mo_LOL<-cv_lol_project_motion_features(tod_motion_features,Y,r,logicalTrain)
-tfr_death12mo_LOL<-cv_lol_project_motion_features(tfr_motion_features,Y,r,logicalTrain)
-
-
-Y<-as.factor(patient_clinical_data$favorable_12mo)
-tod_fav12mo_LOL<-cv_lol_project_motion_features(tod_motion_features,Y,r,logicalTrain)
-tfr_fav12mo_LOL<-cv_lol_project_motion_features(tfr_motion_features,Y,r,logicalTrain)
-
-
-Y<-as.factor(patient_clinical_data$gose_12mo)
-tod_GOSE12mo_LOL<-cv_lol_project_motion_features(tod_motion_features,r,3,logicalTrain)
-tfr_GOSE12mo_LOL<-cv_lol_project_motion_features(tfr_motion_features,r,3,logicalTrain)
