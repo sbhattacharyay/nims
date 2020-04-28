ef39420af7500f5394e37852fb4a0d752efab416
shubhayubh <60406729+sbhattacharyay@users.noreply.github.com>
2020-04-27 19:14:20 -0700

    add original recording scripts

---
+++ scripts/accel_recording_scripts/start_recordingRA.py
@@ -0,0 +1,170 @@
+import datetime
+import time
+import thread
+import pexpect
+
+s6 = "24:71:89:07:75:03"  #RA
+
+classifier = []
+
+stream6 = pexpect.spawn('gatttool -I')
+
+log = open("logRA.txt","w")
+
+def input_thread(classifier):
+        while True:
+                c =  raw_input()
+                classifier.append(c)
+
+
+def ConnectSensors():
+        
+        stream6.sendline('connect '+s6)
+        time.sleep(1)
+
+        try:
+                stream6.expect('Connection successful',timeout=5)
+                
+        except pexpect.TIMEOUT, e:
+                log.write("\n***ERROR***\n")
+                log.write(e[0])
+                log.write("\n")
+                index = e[0].find("connect")+8
+                print"There was an issue connecting to sensor",e[0][index:index+17]
+                return False
+        print"RA sensor connected"
+
+def DisconnectSensors():
+        stream6.sendline('disconnect '+s6)
+        time.sleep(1)
+
+def SetParameters():
+        stream6.sendline('char-write-cmd 0x3c 38:02') #turn on x,y,z accelerometers, range = 8G
+        time.sleep(.5)
+
+        stream6.sendline('char-write-cmd 0x3e 0A') 
+        time.sleep(1)
+
+def TurnOnNotifs():
+        stream6.sendline('char-write-cmd 0x3a 01:00') 
+
+def TurnOffNotifs():
+        stream6.sendline('char-write-cmd 0x3a 00:00') 
+        
+def main():
+        print"QMACP Recording Script v2.1"
+        print"created by John Rattray\n"
+
+        print"Attempting to connect to sensors .."
+        if (ConnectSensors()==False):
+                log.close()
+                return
+        
+                
+        SetParameters()
+        
+
+        print "3"
+        time.sleep(1)
+        print "2"
+        time.sleep(1)
+        print "1"
+        time.sleep(1)
+        print "Recording..."
+        print "Press Ctrl+C to end recording"
+
+        TurnOnNotifs()        
+
+        file = open("raw_dataRA.txt","w")
+        start = time.time()
+        count = 0
+        c = '1'
+        thread.start_new_thread(input_thread, (classifier,))
+        try:
+                while True:
+                        
+                        if classifier:
+                                c  = classifier[0]
+                                classifier.pop()
+                                                
+                        try:
+                                stream6.expect("value:", timeout=1)
+                                ts6 = datetime.datetime.now().strftime("%H:%M:%S:%f")
+                                file.write("6 "+stream6.buffer[1:54]+" "+c+" "+ts6+ "\n")
+                        except pexpect.TIMEOUT, e:
+                                log.write("\n***ERROR***\n")
+                                log.write(e[0])
+                                log.write("\n")
+                                index = e[0].find("LE")-26
+                                print"Connection was dropped with",e[0][index:index+17]
+                                TurnOffNotifs()
+                                DisconnectSensors()
+                                print"Attempting to reconnect"                        
+                                while (ConnectSensors()==False):
+                                        print"Attempting to reconnect"
+                                        DisconnectSensors()
+                                SetParameters()
+                                TurnOnNotifs()
+                                print"Recording..."
+                                
+                        
+        except KeyboardInterrupt:
+                pass
+
+
+        print time.time() - start
+        TurnOffNotifs()
+        
+        file.close()
+        log.close()
+
+        ##convert all saved data
+        file = open("raw_dataRA.txt",'r')
+        file_out = open("dataRA.txt",'w')
+        for line in file:
+
+                gx = int(line [5:7]+line[2:4],16)
+                if gx > 0x7FFF:
+                        gx-=0x10000
+                gx = gx/65536.0/500.0
+                gy = int(line [11:13]+line[8:10],16)
+                if gy > 0x7FFF:
+                        gy-=0x10000
+                gy = gy/65536.0/500.0
+                gz = int(line[17:19]+line[14:16],16)
+                if gz > 0x7FFF:
+                        gz-=0x10000
+                gz = gz/65536.0/500.0
+
+                x = int(line [23:25]+line[20:22],16)
+                if x > 0x7FFF:
+                        x-=0x10000
+                x = x/4096.0 #32768/2 since range is 8G
+                y = int(line [29:31]+line[26:28],16)
+                if y > 0x7FFF:
+                        y-=0x10000
+                y = y/4096.0 #32768/2 since range is 8G
+                z = int(line[35:37]+line[32:34],16)
+                if z > 0x7FFF:
+                        z-=0x10000
+                z = z/4096.0#32768/2 since range is 8G
+                
+                mx = int(line[41:43]+line[38:40],16)
+                if mx > 0x7FFF:
+                        mx-=0x10000
+
+                my = int(line[47:49]+line[44:46],16)
+                if my > 0x7FFF:
+                        my-=0x10000
+
+                mz = int(line[53:55]+line[50:52],16)
+                if mz > 0x7FFF:
+                        mz-=0x10000
+
+                file_out.write (line[:2]+str(gx)+" "+str(gy)+" "+str(gz)+" "+str(x)+" "+str(y)+" "+str(z)+" "+str(mx)+" "+str(my)+" "+str(mz)+line[55:57]+" "+line[58:73]+"\n")
+
+        file.close()
+        file_out.close()        
+
+
+main()
---
+++ scripts/accel_recording_scripts/start_recordingbed.py
@@ -0,0 +1,170 @@
+import datetime
+import time
+import thread
+import pexpect
+
+s7 = "A0:E6:F8:AE:AA:04"  #bed
+
+classifier = []
+
+stream7 = pexpect.spawn('gatttool -I')
+
+log = open("logbed.txt","w")
+
+def input_thread(classifier):
+        while True:
+                c =  raw_input()
+                classifier.append(c)
+
+
+def ConnectSensors():
+        
+        stream7.sendline('connect '+s7)
+        time.sleep(1)
+
+        try:
+                stream7.expect('Connection successful',timeout=5)                
+                
+        except pexpect.TIMEOUT, e:
+                log.write("\n***ERROR***\n")
+                log.write(e[0])
+                log.write("\n")
+                index = e[0].find("connect")+8
+                print"There was an issue connecting to sensor",e[0][index:index+17]
+                return False
+        print"BED sensor connected"
+
+def DisconnectSensors():
+        stream7.sendline('disconnect '+s7)
+        time.sleep(1)
+
+def SetParameters():
+        stream7.sendline('char-write-cmd 0x3c 38:02') #turn on x,y,z accelerometers, range = 8G
+        time.sleep(.5)        
+
+        stream7.sendline('char-write-cmd 0x3e 0A') 
+        time.sleep(1)
+
+def TurnOnNotifs():
+        stream7.sendline('char-write-cmd 0x3a 01:00') 
+
+def TurnOffNotifs():
+        stream7.sendline('char-write-cmd 0x3a 00:00')        
+        
+def main():
+        print"QMACP Recording Script v2.1"
+        print"created by John Rattray\n"
+
+        print"Attempting to connect to sensors .."
+        if (ConnectSensors()==False):
+                log.close()
+                return
+        
+                
+        SetParameters()
+        
+
+        print "3"
+        time.sleep(1)
+        print "2"
+        time.sleep(1)
+        print "1"
+        time.sleep(1)
+        print "Recording..."
+        print "Press Ctrl+C to end recording"
+
+        TurnOnNotifs()        
+
+        file = open("raw_databed.txt","w")
+        start = time.time()
+        count = 0
+        c = '1'
+        thread.start_new_thread(input_thread, (classifier,))
+        try:
+                while True:
+                        
+                        if classifier:
+                                c  = classifier[0]
+                                classifier.pop()
+                                                
+                        try:
+                                stream7.expect("value:", timeout=1)
+                                ts7 = datetime.datetime.now().strftime("%H:%M:%S:%f")                                
+                                file.write("7 "+stream7.buffer[1:54]+" "+c+" "+ts7+ "\n")                                
+                        except pexpect.TIMEOUT, e:
+                                log.write("\n***ERROR***\n")
+                                log.write(e[0])
+                                log.write("\n")
+                                index = e[0].find("LE")-26
+                                print"Connection was dropped with",e[0][index:index+17]
+                                TurnOffNotifs()
+                                DisconnectSensors()
+                                print"Attempting to reconnect"                        
+                                while (ConnectSensors()==False):
+                                        print"Attempting to reconnect"
+                                        DisconnectSensors()
+                                SetParameters()
+                                TurnOnNotifs()
+                                print"Recording..."
+                                
+                        
+        except KeyboardInterrupt:
+                pass
+
+
+        print time.time() - start
+        TurnOffNotifs()
+        
+        file.close()
+        log.close()
+
+        ##convert all saved data
+        file = open("raw_databed.txt",'r')
+        file_out = open("databed.txt",'w')
+        for line in file:
+
+                gx = int(line [5:7]+line[2:4],16)
+                if gx > 0x7FFF:
+                        gx-=0x10000
+                gx = gx/65536.0/500.0
+                gy = int(line [11:13]+line[8:10],16)
+                if gy > 0x7FFF:
+                        gy-=0x10000
+                gy = gy/65536.0/500.0
+                gz = int(line[17:19]+line[14:16],16)
+                if gz > 0x7FFF:
+                        gz-=0x10000
+                gz = gz/65536.0/500.0
+
+                x = int(line [23:25]+line[20:22],16)
+                if x > 0x7FFF:
+                        x-=0x10000
+                x = x/4096.0 #32768/2 since range is 8G
+                y = int(line [29:31]+line[26:28],16)
+                if y > 0x7FFF:
+                        y-=0x10000
+                y = y/4096.0 #32768/2 since range is 8G
+                z = int(line[35:37]+line[32:34],16)
+                if z > 0x7FFF:
+                        z-=0x10000
+                z = z/4096.0#32768/2 since range is 8G
+                
+                mx = int(line[41:43]+line[38:40],16)
+                if mx > 0x7FFF:
+                        mx-=0x10000
+
+                my = int(line[47:49]+line[44:46],16)
+                if my > 0x7FFF:
+                        my-=0x10000
+
+                mz = int(line[53:55]+line[50:52],16)
+                if mz > 0x7FFF:
+                        mz-=0x10000
+
+                file_out.write (line[:2]+str(gx)+" "+str(gy)+" "+str(gz)+" "+str(x)+" "+str(y)+" "+str(z)+" "+str(mx)+" "+str(my)+" "+str(mz)+line[55:57]+" "+line[58:73]+"\n")
+
+        file.close()
+        file_out.close()        
+
+
+main()
