7c9d096febf195632853ff3856a26b1b9e2e3d74
shubhayubh <60406729+sbhattacharyay@users.noreply.github.com>
2020-05-07 10:53:12 -0700

    update classification function to code outcomes as numeric

--- scripts/functions/predict_caret_models.R
+++ scripts/functions/predict_caret_models.R
@@ -2,7 +2,7 @@ predict_caret_models<-function(X_models,CV_set,Y,IdxSet){
   
   tempList <- lapply(X_models, function(x) x%>%predict(CV_set,type = "prob")%>%dplyr::select(2))
   tempMat<-list.cbind(tempList)
-  tempDF<-cbind(Y[IdxSet],as.data.frame(tempMat))
+  tempDF<-cbind((as.numeric(Y[IdxSet])-1),as.data.frame(tempMat))
   names(tempDF)<-c('ground_truth',names(X_models))
   
   return(tempDF)
