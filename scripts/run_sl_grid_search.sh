d776b23d9abe8fec9dfd35d75eb4775bf98ec097
Eshan Joshi <eshanjoshi@Eshans-MBP-2.attlocal.net>
2020-05-14 17:08:05 -0500

    add grid search

---
+++ scripts/run_sl_grid_search.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#!/bin/bash
+#SBATCH --job-name=rscript
+#SBATCH --time=0:0:10
+#SBATCH --partition=debug
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#
+#---------------------------------------------------------------------
+# SLURM job script to run serial R
+#---------------------------------------------------------------------
+ 
+ml R
+ml # confirm modules used
+Rscript ./sl_grid_search.R
