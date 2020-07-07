436e84a999e8e26e6ff80fe37ccba7a052cd6e12
Matt Wang <mwang103@jhu.edu>
2020-07-07 02:46:21 -0400

    edits

--- scripts/findbefore_gcs.m
+++ scripts/findbefore_gcs.m
@@ -16,9 +16,11 @@ load('../all_motion_feature_data/featureset_ptidx_timestamp.mat')
 % Change here
 time_before = 5/60/24;
 [~,~,gcs_data] = xlsread('../clinical_data/GCS_table.xlsx');
+header = gcs_data(1,:);
 gcs_pt = cell2mat(gcs_data(2:end,1));
 gcs_date = datenum(gcs_data(2:end,2), 'dd/mm/yyyy') + cell2mat(gcs_data(2:end,9));
 accel_overlap = logical(cell2mat(gcs_data(2:end,8)));
+gcs_data = gcs_data(accel_overlap,:);
 gcs_pt = gcs_pt(accel_overlap);
 gcs_date = gcs_date(accel_overlap);
 
@@ -26,12 +28,17 @@ gcs_date = gcs_date(accel_overlap);
 accel_conv = cell2mat(clinical_data(2:70,2));
 study_conv = cell2mat(clinical_data(2:70,1));
 
+% for j = 1:length(ptIdx)
+%     index = find(accel_conv == ptIdx(j));
+%     if isempty(index)
+%         continue
+%     else
+%         ptIdx(j) = study_conv(index);
+%     end
+% end
+
 for j = 1:length(ptIdx)
-    if isempty(find(accel_conv == ptIdx(j)))
-        continue
-    else
-        ptIdx(j) = find(accel_conv == ptIdx(j));
-    end
+   ptIdx(j) = study_conv(ptIdx(j));
 end
 
 % for k = 1:length(gcs_pt)
@@ -60,6 +67,10 @@ for k = 1:length(gcs_pt)
        ptIdx_uniqueid(find(tot_index)) = curr_unique_id;
    end
 end
-sum(ptIdx_uniqueid)
-unique(ptIdx_uniqueid)
 
+uniqueid = table2cell(array2table(uniqueid'));
+gcs_data = horzcat(gcs_data, uniqueid);
+header = horzcat(header, {'Unique ID'});
+gcs_data = vertcat(header, gcs_data);
+
+save('../all_motion_feature_data/gcs_featureset_id.mat', 'gcs_data', 'ptIdx', 'ptIdx_uniqueid', 'timeStamp')
